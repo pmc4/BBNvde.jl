53635263550d78ab1cb85260885bb13b7abef8fb
Documenter.jl <documenter@juliadocs.github.io>
2023-09-25 15:19:28 +0000

    build based on ea739f3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BBNvde","category":"page"},{"location":"#BBNvde","page":"Home","title":"BBNvde","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BBNvde.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BBNvde]","category":"page"}]
+}
